e0529109bef87df27b5990cd54519fc5d736bb97
Dany Pineault <danpin7679@gmail.com>
2025-07-01 06:38:54 -0400

    Course2

--- src/courses/views.py
+++ src/courses/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
-# Create your views here.
+from .models import Course
+
+
+def course_list(request):
+    player_list = Course.objects.all
+    template = loader.get_template("courses/list.html")
+    context = {"courses": course_list}
+    return HttpResponse(template.render(context, request))
+
