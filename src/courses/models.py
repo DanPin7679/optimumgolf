4bf75f099732a4032b8f800acf7661b344fb8103
Dany Pineault <danpin7679@gmail.com>
2025-07-01 06:28:00 -0400

    Courses1

---
+++ src/courses/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+class AccessRequirement(models.TextChoices):
+    ANYONE = "any", "Anyone"
+    EMAIL_REQUIRED = "email", "Email required"
+
+class PublishStatus(models.TextChoices):
+    PUBLISHED = "publish", "Published"
+    COMING_SOON = "soon", "Coming Soon"
+    DRAFT = "draft", "Draft"
+
+
+def handle_upload(instance, filename):
+    return f"{filename}"
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    # uuid = models.UUIDField(default=uuid.uuid1, unique=True)
+    public_id = models.CharField(max_length=130, blank=True, null=True, db_index=True)
+    image = models.ImageField(upload_to=handle_upload, blank=True, null=True)
+    # image = CloudinaryField(
+    #     "image", 
+    #     null=True, 
+    #     public_id_prefix=get_public_id_prefix,
+    #     display_name=get_display_name,
+    #     tags=["course", "thumbnail"]
+    # )
+    access = models.CharField(
+        max_length=5, 
+        choices=AccessRequirement.choices,
+        default=AccessRequirement.EMAIL_REQUIRED
+    )
+    status = models.CharField(
+        max_length=10, 
+        choices=PublishStatus.choices,
+        default=PublishStatus.DRAFT
+        )
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+    @property
+    def is_published(self):
+        return self.status == PublishStatus.PUBLISHED
